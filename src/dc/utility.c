db5520e3e56e8b4da1a66bbbfa3fdc0395524ad5
cpasjuste <cpasjuste@gmail.com>
2020-02-04 11:04:42 +0100

    dc: cleanup

--- src/dc/utility.c
+++ src/dc/utility.c
@@ -3,6 +3,15 @@
 //
 
 #include <kos.h>
+#include "utility.h"
+
+extern FileList s_file_list;
+
+extern void add_file(FileList *list, File *entry, int sort);
+
+extern FileList *get_dir(const char *path);
+
+extern void free_dir(FileList *list);
 
 int file_exists(const char *fn) {
     file_t f;
@@ -57,6 +66,45 @@ char *read_file(const char *file) {
     return buffer;
 }
 
+FileList *get_dir(const char *path) {
+
+    dirent_t *ent;
+    file_t fd;
+    File *entry;
+
+    free_dir(&s_file_list);
+    memset(&s_file_list, 0, sizeof(FileList));
+    strncpy(s_file_list.path, path, MAX_PATH);
+
+    if ((fd = fs_open(path, O_RDONLY | O_DIR)) != FILEHND_INVALID) {
+        while ((ent = fs_readdir(fd)) != NULL) {
+
+            // skip "."
+            if (ent->name[0] == '.') {
+                continue;
+            }
+
+            entry = (File *) malloc(sizeof(File));
+            strncpy(entry->name, ent->name, MAX_PATH);
+            if (path[strlen(path) - 1] != '/') {
+                snprintf(entry->path, MAX_PATH - 1, "%s/%s", path, ent->name);
+            } else {
+                snprintf(entry->path, MAX_PATH - 1, "%s%s", path, ent->name);
+            }
+
+            struct stat st;
+            if (stat(entry->path, &st) == 0) {
+                entry->type = S_ISDIR(st.st_mode) ? TYPE_DIR : TYPE_FILE;
+            }
+
+            add_file(&s_file_list, entry, SORT_BY_NAME_AND_FOLDER);
+        }
+        fs_close(fd);
+    }
+
+    return &s_file_list;
+}
+
 int flash_get_region() {
 
     int start, size;
@@ -84,7 +132,6 @@ int flash_get_region() {
         return FLASHROM_REGION_UNKNOWN;
     }
 }
-
 
 int is_hacked_bios() {
     return (*(uint16 *) 0xa0000000) == 0xe6ff;
